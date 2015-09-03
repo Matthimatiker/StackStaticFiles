bd7809dbae45e42b4e64fc6c002d9484ac66f9ac
Matthias Molitor <mmo@webfactory.de>
2015-09-03 18:43:28 +0200

    improved comments

--- src/StaticFiles.php
+++ src/StaticFiles.php
@@ -6,19 +6,35 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 
+/**
+ * Middleware that serves files from a provided directory.
+ *
+ * Delegates to a decorated kernel, if a requested file does not exist.
+ */
 class StaticFiles implements HttpKernelInterface
 {
     /**
+     * The decorated kernel.
+     *
      * @var HttpKernelInterface
      */
     protected $innerKernel = null;
 
+    /**
+     * Path to the directory that is searched for files.
+     *
+     * @var string
+     */
     protected $webDir = null;
 
+    /**
+     * @param HttpKernelInterface $innerKernel
+     * @param string $webDir
+     */
     public function __construct(HttpKernelInterface $innerKernel, $webDir)
     {
         $this->innerKernel = $innerKernel;
-        $this->webDir = $webDir;
+        $this->webDir      = $webDir;
     }
 
     /**
@@ -31,12 +47,8 @@ class StaticFiles implements HttpKernelInterface
      * @param int $type The type of the request
      *                         (one of HttpKernelInterface::MASTER_REQUEST or HttpKernelInterface::SUB_REQUEST)
      * @param bool $catch Whether to catch exceptions or not
-     *
      * @return Response A Response instance
-     *
      * @throws \Exception When an Exception occurs during processing
-     *
-     * @api
      */
     public function handle(Request $request, $type = self::MASTER_REQUEST, $catch = true)
     {
